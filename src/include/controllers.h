5bf7de4726c290e2a3e7646e6cc8c42e5543ead5
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2022-03-27 11:53:49 -0500

    Merge pull request #2
    * add repositories header file
    
    * add models header file
    
    * add dtos header file
    
    * add entities header file
    
    * update CMakeLists.txt
    
    * add kitty service
    
    * add kitty controller

---
+++ src/include/controllers.h
@@ -0,0 +1,10 @@
+//
+// Created by Matthew Crowell on 3/27/22 at 11:49 AM.
+//
+
+#ifndef KITTIES02_CONTROLLERS_H
+#define KITTIES02_CONTROLLERS_H
+
+#include "../controllers/kitty_controller.hpp"
+
+#endif //KITTIES02_CONTROLLERS_H
