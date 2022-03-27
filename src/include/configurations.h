a4ad6675e6e51a16b30a3afff1000922f107c99f
Matthew Crowell <>
2022-03-27 12:49:23 -0500

    add configurations header file

---
+++ src/include/configurations.h
@@ -0,0 +1,12 @@
+//
+// Created by Matthew Crowell on 3/27/22 at 12:37 PM.
+//
+
+#ifndef KITTIES02_CONFIGURATIONS_H
+#define KITTIES02_CONFIGURATIONS_H
+
+#include "../src/config/app_component.hpp"
+#include "../src/config/database_component.hpp"
+#include "../src/config/swagger_component.hpp"
+
+#endif //KITTIES02_CONFIGURATIONS_H
