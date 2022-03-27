63e4c3ac0a47165439391e41a3b885af82fb919a
Matthew Crowell <>
2022-03-27 10:12:04 -0500

    add repositories header file

---
+++ src/include/repositories.h
@@ -0,0 +1,10 @@
+//
+// Created by Matthew Crowell on 3/27/22 at 10:11 AM.
+//
+
+#ifndef KITTIES02_REPOSITORIES_H
+#define KITTIES02_REPOSITORIES_H
+
+#include "../repos/kitty_repository.hpp"
+
+#endif //KITTIES02_REPOSITORIES_H
